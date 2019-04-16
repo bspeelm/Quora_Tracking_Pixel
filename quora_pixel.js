967d810087dc90441cfd4845624d85e4de3a8a1a
Bryan Speelman <bryan.speelman@coveros.com>
2019-04-16 19:15:32 -0400

    Update pixel to identify domain and apply proper id

--- quora_pixel.js
+++ quora_pixel.js
@@ -1,7 +1,6 @@
 <!-- DO NOT MODIFY -->
 <!-- Quora Pixel Code (JS Helper) -->
 
-<script>
 !function(q,e,v,n,t,s){
   if(q.qp) return; n=q.qp=function(){
     n.qp?n.qp.apply(n,arguments):n.queue.push(arguments);
@@ -15,15 +14,6 @@
 qp('init', '96781d2e7b7243839b32d401857dc978');
 qp('track', 'ViewContent');
 alert("Quora Running");
-</script>
-
-<noscript>
-  <img 
-    height="1" 
-    width="1" 
-    style="display:none" 
-    src="https://q.quora.com/_/ad/96781d2e7b7243839b32d401857dc978/pixel?tag=ViewContent&noscript=1"
-  />
-</noscript>
 
 <!-- End of Quora Pixel Code -->
+<script>qp('track','<Generic>');</script>
